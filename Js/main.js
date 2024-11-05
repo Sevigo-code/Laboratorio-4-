a4bfa32940c8fe9d17237ba65abf7a38492c6438
Sevigo-code <juancamilocartagena7@gmail.com>
2024-11-04 22:20:27 -0500

    dark mode basico implementado

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,25 @@
 function saludar() {
     console.log("Hola mundo")
 }
+
+let isBlack = true;
+const button = document.getElementsByTagName("button")[0];
+
+let text = document.getElementsByTagName("h1")[0];
+
+
+button.addEventListener("click", function() {
+
+    if(isBlack) {
+        document.body.style.backgroundColor = "black";
+        text.style.color = "white"
+    }
+
+    else {
+        document.body.style.backgroundColor = "antiquewhite"
+        text.style.color = "black"
+    }
+    
+    isBlack = !isBlack
+
+})
